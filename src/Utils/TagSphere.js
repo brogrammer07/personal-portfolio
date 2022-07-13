4ed3d4f6736a0e6842a4700dc9e818f0fa13c7f2
Tushar Tomar <tomartushar5@gmail.com>
2022-07-13 12:45:20 +0530

    Skills

--- src/Utils/TagSphere.js
+++ src/Utils/TagSphere.js
@@ -1,25 +1,9 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import TagCloud from "TagCloud";
+import { mySkillsData } from "../Assests/data";
 const TagSphere = () => {
-  console.log("yo");
+  const [myTags, setMyTags] = useState(mySkillsData.words);
   useEffect(() => {
-    const myTags = [
-      "JavaScript",
-      "CSS",
-      "HTML",
-      "Vscode",
-      "Figma",
-      "React",
-      "NextJS",
-      "git",
-      "Firebase",
-      "NodeJS",
-      "Express",
-      "MongoDB",
-      "Mongoose",
-      "UI/UX",
-      "Figma",
-    ];
     TagCloud(".tagcloud", myTags, {
       radius: 250,
       itemClass: "text-secondary text-2xl font-bold ",
