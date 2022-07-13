6fde4366bcefcd5ac77dea81af9887a921890cc8
Tushar Tomar <tomartushar5@gmail.com>
2022-07-13 12:43:03 +0530

    Skills

--- src/Utils/TagSphere.js
+++ src/Utils/TagSphere.js
@@ -1,30 +1,30 @@
 import React, { useEffect } from "react";
 import TagCloud from "TagCloud";
 const TagSphere = () => {
-  const myTags = [
-    "JavaScript",
-    "CSS",
-    "HTML",
-    "Vscode",
-    "Figma",
-    "React",
-    "NextJS",
-    "git",
-    "Firebase",
-    "NodeJS",
-    "Express",
-    "MongoDB",
-    "Mongoose",
-    "UI/UX",
-    "Figma",
-  ];
   console.log("yo");
   useEffect(() => {
+    const myTags = [
+      "JavaScript",
+      "CSS",
+      "HTML",
+      "Vscode",
+      "Figma",
+      "React",
+      "NextJS",
+      "git",
+      "Firebase",
+      "NodeJS",
+      "Express",
+      "MongoDB",
+      "Mongoose",
+      "UI/UX",
+      "Figma",
+    ];
     TagCloud(".tagcloud", myTags, {
       radius: 250,
       itemClass: "text-secondary text-2xl font-bold ",
     });
-  }, []);
+  }, [myTags]);
   return <div className="tagcloud"></div>;
 };
 
